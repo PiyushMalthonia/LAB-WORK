49733130de2f2bec99aed4a2816316e78f81fe3c
PiyushMalthonia <146948842+PiyushMalthonia@users.noreply.github.com>
2024-01-05 11:24:22 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int avg(int[],int);
+void main()
+{
+    int mark[]={77,88,99,88,99};
+    int size;
+    size=sizeof(mark)/sizeof(mark[0]);
+    average=avg(mark,size);
+    printf("average=%d\n",average);
+}
+int avg(int mark[1],int size)
+{
+int i,sum
+}
--- calculator.c
+++ calculator.c
@@ -1,35 +1,35 @@
 #include<stdio.h>
 void main()
 {
-  int a,b,c;
-  printf("Enter two numbers");
-  scanf("%d%d",&a,&b);
-  int x;
-  printf("#1=addition 2=subtraction 3=multiplication 4=division");  
-  scanf("%d",&x);
-  if(x==1)
-  {
-    c=a+b;
-    printf("addition:%d",c);
-  }
-  else if(x==2)
-  {
-    c=a-b;
-    printf("subtraction:%d",c);
-  }
-  else if(x==3)
-  {
-  c=a*b;
-  printf("multiplication:%d",c);
-  }
-  else if(x==4)
-  {
-    if(b==0)
-    printf("infinity");
-  }
-  else
-  {
-   c=a/b;
-   printf("division:%d",c);
-  }
-}
+    int a,b,c;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    int x;
+    printf("1=Addition 2=Subtraction 3=Multiplication 4=Division");
+    scanf("%d",&x);
+    if(x==1)
+    {
+        c=a+b;
+        printf("Addition: %d",c)
+    }
+    else if(x==2)
+    {
+        c=a-b;
+        printf("Subtraction: %d",c);
+    }
+    else if(x==3)
+    {
+        c=a*b;
+        printf("Multiplication: %d",c);
+    }
+    else if(x==4)
+    {
+        if(b==0)
+            printf("Infinity");
+    }
+        else
+        {
+            c=a/b;
+            printf("Division: %d",c);
+        }
+    }
