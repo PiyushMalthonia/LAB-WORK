9fc874acda9a74e7b0bc07d67341483a53eeae96
PiyushMalthonia <146948842+PiyushMalthonia@users.noreply.github.com>
2024-01-05 11:22:28 +0530

    Add files via upload

---
+++ circleAndrectanglehavesamearea.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float r,l,b,area,perimeter;
+    r=sqrt((44*56)/3.14);
+    perimeter=2*3.14*r;
+    printf("Perimeter of circle is %f\n",perimeter);
+    return 0;
+    
+
+}
---
+++ dodge.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{ 
+int a=3,b=4,C;
+c=a++ + ++a + a++ + ++a;
+printf("%d",c);
+}
