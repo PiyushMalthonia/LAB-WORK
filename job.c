6e4413354459a7008dd0590f79ba31dea8e21744
PiyushMalthonia <146948842+PiyushMalthonia@users.noreply.github.com>
2024-01-05 11:20:22 +0530

    Add files via upload

---
+++ job.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int age;
+    printf("enter the age");
+    scanf("%d",&age);
+    if(age>=19 && age<=50)
+    if(age>=18 && age<=25)
+    {
+        printf("eligible for grade a job");
+    }
+    else
+    {
+        printf("eligible for grade b job");
+    }
+    else
+    {
+    printf("young or not eligible for job");
+    }
+}
